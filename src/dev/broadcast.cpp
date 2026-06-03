dc49b2e16bfbc0f66dc752a8386863f0f636df1e
Brad Bell <bradbell@seanet.com>
2026-06-02 20:53:06 -0700

    TensorType: broadcast.cpp: white space edit.

--- src/dev/broadcast.cpp
+++ src/dev/broadcast.cpp
@@ -59,65 +59,67 @@ namespace ad_tensor { namespace dev {
     c10::ArrayRef<long> broadcast(
         bool lock             ,
         const at::Tensor& res ,
-        const at::Tensor& arg ) {
-    //
-    // locked
-    thread_local bool locked = false;
-    //
-    // dim
-    thread_local vector<long> dim;
-    //
-    // locked
-    if( ! lock )
-    {   assert( locked && "broadcast: "
-            "a call with lock false was not preceded by a call with lock true"
-        );
-        locked = false;
-        return c10::ArrayRef<long>();
-    }
-    assert( ! locked && "broadcast: "
-        "attempt to get a lock while another call is holding its lock"
-    );
-    locked = true;
-    //
-    //
-    // dim
-#ifdef NDEBUG
-    dim.resize(0)
-#else
-    size_t cap = dim.capacity();
-    dim.resize(0);
-    assert( dim.capacity() == cap && "broadcast: "
-        "std::vector::resize to zero changed capacity"
-    );
-# endif
-    //
-    // check for case where tensor shapes are equal
-    c10::ArrayRef<long> res_shape = res.sizes();
-    c10::ArrayRef<long> arg_shape = arg.sizes();
-    if( res_shape.equals( arg_shape) ) {
-        return c10::ArrayRef<long> ();
-    }
-    //
-    // dim
-    size_t res_len = res_shape.size();
-    size_t arg_len = arg_shape.size();
-    assert( arg_len <= res_len && "binary_broadcast: "
-        "arg shape is longer than res shape"
-    );
-    for(size_t i = 0; i < res_len; ++i)
-    {   size_t res_index  = res_len - i - 1;
-        if( i < arg_len )
-        {   size_t arg_index = arg_len - i - 1;
-            if( arg_shape[arg_index] != res_shape[res_index] )
-            {   assert( arg_shape[arg_index] == 1 && "binary_broadcast: "
-                    "an arg size is not one and different from its res size"
-                );
-                dim.push_back( static_cast<long>(res_index) );
-            }
-        } else if( 1 < res_shape[res_index] ) {
-            dim.push_back(res_index);
+        const at::Tensor& arg
+    ) {
+        //
+        // locked
+        thread_local bool locked = false;
+        //
+        // dim
+        thread_local vector<long> dim;
+        //
+        // locked
+        if( ! lock )
+        {   assert( locked && "broadcast: a call with lock false "
+                "was not preceded by a call with lock true"
+            );
+            locked = false;
+            return c10::ArrayRef<long>();
         }
+        assert( ! locked && "broadcast: "
+            "attempt to get a lock while another call is holding its lock"
+        );
+        locked = true;
+        //
+        //
+        // dim
+#ifdef NDEBUG
+        dim.resize(0)
+#else
+        size_t cap = dim.capacity();
+        dim.resize(0);
+        assert( dim.capacity() == cap && "broadcast: "
+            "std::vector::resize to zero changed capacity"
+        );
+# endif
+        //
+        // check for case where tensor shapes are equal
+        c10::ArrayRef<long> res_shape = res.sizes();
+        c10::ArrayRef<long> arg_shape = arg.sizes();
+        if( res_shape.equals( arg_shape) ) {
+            return c10::ArrayRef<long> ();
+        }
+        //
+        // dim
+        size_t res_len = res_shape.size();
+        size_t arg_len = arg_shape.size();
+        assert( arg_len <= res_len && "binary_broadcast: "
+            "arg shape is longer than res shape"
+        );
+        for(size_t i = 0; i < res_len; ++i)
+        {   size_t res_index  = res_len - i - 1;
+            if( i < arg_len )
+            {   size_t arg_index = arg_len - i - 1;
+                if( arg_shape[arg_index] != res_shape[res_index] )
+                {   assert( arg_shape[arg_index] == 1 && "binary_broadcast: "
+                        "an arg size is different from its res size and not 1"
+                    );
+                    dim.push_back( static_cast<long>(res_index) );
+                }
+            } else if( 1 < res_shape[res_index] ) {
+                dim.push_back(res_index);
+            }
+        }
+        return c10::ArrayRef<long> (dim);
     }
-    return c10::ArrayRef<long> (dim);
-} } }
+}  }
