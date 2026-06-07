3007677551ff141f3300202a6a269bce13b81244
Brad Bell <bradbell@seanet.com>
2026-06-07 06:49:43 -0700

    main: test the trnaspose operator.

--- tests/adten/transpose_op.cpp
+++ tests/adten/transpose_op.cpp
@@ -9,7 +9,7 @@
 #include <ad_tensor/adten.hpp>
 #include <ad_tensor/dev/to_string.hpp>
 //
-TEST(tests, adten_matmul_op)  {
+TEST(tests, adten_transpose_op)  {
     using ad_tensor::adten_t;
     using ad_tensor::adfn_t;
     using ad_tensor::options_t;
@@ -22,13 +22,11 @@ TEST(tests, adten_matmul_op)  {
     // p
     vector<Tensor> p;
     //
-    double x00 = 1.0, x01 = 2.0, x10 = 3.0, x11 = 4.0;
-    //
     // x
     vector<Tensor> x;
     x.push_back( torch::tensor( { 
-        {x00, x01}, 
-        {x10, x11} 
+        {1.0, 2.0}, 
+        {3.0, 4.0} 
     } ) );
     //
     // ap, ax
@@ -36,7 +34,7 @@ TEST(tests, adten_matmul_op)  {
     //
     // ay
     vector<adten_t> ay;
-    ay.push_back( ax[0].matmul( ax[0] ) );
+    ay.push_back( ax[0].transpose(0, 1) );
     //
     // y = f(p)
     adfn_t f = adten_t::stop_recording(ay);
@@ -51,8 +49,8 @@ TEST(tests, adten_matmul_op)  {
     EXPECT_EQ( y.size(), ay.size() );
     //
     Tensor check = torch::tensor( {
-        {x00*x00 + x01*x10, x00*x01 + x01*x11 },
-        {x10*x00 + x11*x10, x10*x01 + x11*x11 }
+        {1.0, 3.0}, 
+        {2.0, 4.0} 
     } );
     bool equal = y[0].equal( check );
     EXPECT_TRUE( equal );
@@ -60,29 +58,25 @@ TEST(tests, adten_matmul_op)  {
     // dx
     vector<Tensor> dx;
     dx.push_back( torch::tensor( {
-        {0.0, 0.0},
-        {0.0, 1.0}
+        {5.0, 6.0},
+        {7.0, 8.0}
     } ) );
     //
     // dy
     vector<Tensor> dy = f.forward_der(par_all, var_all, dx, options);
     check = torch::tensor( {
-            {0.0,       x01},
-            {x10, 2.0 * x11}
+        {5.0, 7.0},
+        {6.0, 8.0}
     } );
     equal = dy[0].equal( check );
     EXPECT_TRUE( equal );
-    // dy, dx
-    dy[0] = torch::tensor( {
-        {1.0, 0.0}, {0.0, 0.0}
-    } );
+    //
+    // dx
     dx    = f.reverse_der(par_all, var_all, dy, options);
     check = torch::tensor( {
-        {2.0 * x00, x10},
-        {x01,       0.0}
+        {5.0, 6.0},
+        {7.0, 8.0}
     } );
-    std::cout << "dx[0] = " << dx[0] << "\n";
-    std::cout << "check = " << check << "\n";
     equal = dx[0].equal( check );
     EXPECT_TRUE( equal );
 }
