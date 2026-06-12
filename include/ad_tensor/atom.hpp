8ba9f629fb0cad2ecc0161f0d2629dc3791c1b69
Brad Bell <bradbell@seanet.com>
2026-06-12 10:08:24 -0700

    atom: start new branch for working on atomic functions.

---
+++ include/ad_tensor/atom.hpp
@@ -0,0 +1,137 @@
+#pragma once
+// SPDX-License-Identifier: EPL-2.0 OR GPL-2.0-or-later
+// SPDX-FileCopyrightText: Bradley M. Bell <bradbell@seanet.com>
+// SPDX-FileContributor: 2026 Bradley M. Bell
+// ----------------------------------------------------------------------------
+/*
+{xrst_begin atom dev}
+
+Atomic Functions
+################
+{xrst_literal ,
+    BEGIN_ATOM_T, END_ATOM_T
+}
+
+{xrst_end atom}
+*/
+#include <torch/torch.h>
+#include <ad_tensor/vector.hpp>
+#include <ad_tensor/adfn.hpp>
+//
+namespace ad_tensor {  // BEGIN_AD_TENSOR_NAMESPACE
+// --------------------------------------------------------------------------
+// atom_t
+class atom_t {
+public:
+    //
+    // forward_var_t, ad_forward_var_t
+    typedef vector<at::Tensor> (*forward_var_t) (
+        size_t             call_info ,
+        vector<at::Tensor> domain
+    );
+    typedef vector<adten_t> (*ad_forward_var_t) (
+        size_t             call_info ,
+        vector<adten_t>    domain
+    );
+    //
+    // forward_der_t, ad_forward_der_t
+    typedef vector<at::Tensor> (*forward_der_t) (
+        size_t             call_info ,
+        vector<at::Tensor> domain ,
+        vector<at::Tensor> domain_der
+    );
+    typedef vector<adten_t> (*ad_forward_der_t) (
+        size_t             call_info ,
+        vector<adten_t>    domain ,
+        vector<adten_t>    domain_der
+    );
+    //
+    // reverse_der_t, ad_reverse_der_t
+    typedef vector<at::Tensor> (*reverse_der_t) (
+        size_t             call_info ,
+        vector<at::Tensor> domain    ,
+        vector<at::Tensor> range_der
+        );
+    typedef vector<adten_t> (*ad_reverse_der_t) (
+        size_t             call_info ,
+        vector<adten_t>    domain    ,
+        vector<adten_t>    range_der
+    );
+private:
+    // m_name
+    std::string m_name;
+    //
+    // m_forward_var, m_forward_der, m_reverse_der
+    forward_var_t     m_forward_var;
+    forward_der_t     m_forward_der;
+    reverse_der_t     m_reverse_der;
+    //
+    // m_ad_forward_var, m_ad_forward_der, m_ad_reverse_der
+    ad_forward_var_t     m_ad_forward_var;
+    ad_forward_der_t     m_ad_forward_der;
+    ad_reverse_der_t     m_ad_reverse_der;
+public:
+    //
+    // BEGIN_CTOR
+    atom_t(void)
+    : m_name()
+    , m_forward_var(nullptr)
+    , m_forward_der(nullptr)
+    , m_reverse_der(nullptr)
+    , m_ad_forward_var(nullptr)
+    , m_ad_forward_der(nullptr)
+    , m_ad_reverse_der(nullptr)
+    // END_CTOR
+    { }
+    //
+    // Setters
+    void set_name(const std::string& name);
+    void set_forward_var(const forward_var_t& forward_var);
+    void set_forward_der(const forward_der_t& forward_der);
+    void set_reverse_der(const reverse_der_t& reverse_der);
+    void set_ad_forward_var(const ad_forward_var_t& ad_forward_var);
+    void set_ad_forward_der(const ad_forward_der_t& ad_forward_der);
+    void set_ad_reverse_der(const ad_reverse_der_t& ad_reverse_der);
+    //
+    // Getters
+    const std::string& get_name(void) const;
+    const forward_var_t& get_forward_var(void) const;
+    const forward_der_t& get_forward_der(void) const;
+    const reverse_der_t& get_reverse_der(void) const;
+    const ad_forward_var_t& get_ad_forward_var(void) const;
+    const ad_forward_der_t& get_ad_forward_der(void) const;
+    const ad_reverse_der_t& get_ad_reverse_der(void) const;
+
+
+};
+// atom_info_t
+class atom_info_t {
+private:
+    std::shared_mutex m_rw_mutex;
+    vector<atom_t>    m_atom_vec;
+    //
+    // default constructor
+    atom_info_t(void)
+    { }
+public:
+    //
+    // automatic copy constructor
+    atom_info_t(const atom_info_t&) = delete;
+    //
+    //  automatic assignment operator
+    void operator=(const atom_info_t&) = delete;
+    //
+    // singleton
+    static atom_info_t& singleton(void)
+    {   static atom_info_t m_atom_info;
+        return m_atom_info;
+    }
+    //
+    // store
+    size_t store(const atom_t& adfn);
+    //
+    // get
+    const atom_t& get(size_t index);
+};
+
+} // END_AD_TENSOR_NAMESPACE
---
+++ src/atom.cpp
@@ -0,0 +1,94 @@
+// SPDX-License-Identifier: EPL-2.0 OR GPL-2.0-or-later
+// SPDX-FileCopyrightText: Bradley M. Bell <bradbell@seanet.com>
+// SPDX-FileContributor: 2026 Bradley M. Bell
+// ----------------------------------------------------------------------------
+#include<thread>
+#include<chrono>
+#include <ad_tensor/atom.hpp>
+#include <ad_tensor/dev/user_assert.hpp>
+//
+#define SETTER(name) \
+    void atom_t::set_ ## name(const name ## _t& name) { \
+        dev::user_assert( name != nullptr, \
+            "atom " #name " is the nullptr in set_" #name  \
+        ); \
+        m_ ## name = name; \
+    }
+#define GETTER(name) \
+    const atom_t::name ## _t& atom_t::get_ ## name(void) const { \
+        if( m_ ## name == nullptr ) { \
+            std::string msg = m_name + " atom: " #name " has not been set"; \
+            dev::user_assert(false, msg); \
+        } \
+        return m_ ## name; \
+    }
+//
+namespace ad_tensor {
+    //
+    // atom_info_t::store
+    size_t atom_info_t::store(const atom_t& atom) {
+        //
+        // lock, m_rw_mutex
+        size_t count = 0;
+        bool   lock  = false;
+        while( count < 100 && ! lock ) {
+            lock = m_rw_mutex.try_lock();
+            if( ! lock )
+            {   ++count;
+                std::this_thread::sleep_for( std::chrono::milliseconds(10) );
+            }
+        }
+        if( ! lock ) {
+            std::cerr << "atom_info::store: tried for 1 second to get a lock\n";
+#ifndef NDEBUG
+            std::exit(1);
+#else
+            assert(lock);
+#endif
+        }
+        // m_atom_vec
+        m_atom_vec.push_back( atom );
+        //
+        // m_rw_mutex
+        if( lock ) {
+            m_rw_mutex.unlock();
+        }
+        //
+        // atom_id
+        return m_atom_vec.size() - 1;
+    }
+    //
+    // atom_info_t::get
+    const atom_t& atom_info_t::get(size_t atom_id) {
+        std::shared_lock<std::shared_mutex> lock(m_rw_mutex);
+        return m_atom_vec[atom_id];
+    }
+    //
+    // atom_t::set_name
+    void atom_t::set_name(const std::string& name) {
+        dev::user_assert( name != "",
+            "atom set_name: name is the empty string"
+        );
+        m_name = name;
+    }
+    // atom_t:: set_forward_var, set_forward_der, set_reverse_der
+    SETTER(forward_var)
+    SETTER(forward_der)
+    SETTER(reverse_der)
+    //
+    // atom_t:: set_ad_forward_var, set_ad_forward_der, set_ad_reverse_der
+    SETTER(ad_forward_var)
+    SETTER(ad_forward_der)
+    SETTER(ad_reverse_der)
+    //
+    // atom_t:: get_forward_var, get_forward_der, get_reverse_der
+    GETTER(forward_var)
+    GETTER(forward_der)
+    GETTER(reverse_der)
+    //
+    // atom_t:: get_ad_forward_var, get_ad_forward_der, get_ad_reverse_der
+    GETTER(ad_forward_var)
+    GETTER(ad_forward_der)
+    GETTER(ad_reverse_der)
+
+}
