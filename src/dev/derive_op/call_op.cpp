a16d2bacc461243a4922763faa7d5f680409559f
Brad Bell <bradbell@seanet.com>
2026-06-19 10:31:12 -0700

    atom: call_op.cpp: remove one level of indent.

--- src/dev/derive_op/call_op.cpp
+++ src/dev/derive_op/call_op.cpp
@@ -8,87 +8,87 @@
 #include <ad_tensor/dev/plus_minus_equal.hpp>
 #include <ad_tensor/dev/tensor_at_index.hpp>
 //
-namespace ad_tensor { namespace dev {
-    // ------------------------------------------------------------------------
-    // forward_par
-    template<> void call_op_t<adten_t>::forward_par(
-        size_t                       op_index    ,
-        const agraph_t&              agraph      ,
-        const vector<at::Tensor>&    con_vec     ,
-        vector<adten_t>&             par_vec
-    ) const {
-        assert(false && "call_op: ad_forward_par not implemented");
-    }
-    template<> void call_op_t<at::Tensor>::forward_par(
-        size_t                       op_index    ,
-        const agraph_t&              agraph      ,
-        const vector<at::Tensor>&    con_vec     ,
-        vector<at::Tensor>&          par_vec
-    ) const {
-        assert(false && "call_op: forward_par not implemented");
-    }
-    // ------------------------------------------------------------------------
-    // forward_var
-    template<> void call_op_t<adten_t>::forward_var(
-        size_t                       op_index    ,
-        const agraph_t&              agraph      ,
-        const vector<at::Tensor>&    con_vec     ,
-        const vector<adten_t>&       par_vec     ,
-        vector<adten_t>&             var_vec
-    ) const {
-        assert(false && "call_op: ad_forward_var not implemented");
-    }
-    template<> void call_op_t<at::Tensor>::forward_var(
-        size_t                       op_index    ,
-        const agraph_t&              agraph      ,
-        const vector<at::Tensor>&    con_vec     ,
-        const vector<at::Tensor>&    par_vec     ,
-        vector<at::Tensor>&          var_vec
-    ) const {
-        assert(false && "call_op: forward_var not implemented");
-    }
-    // ------------------------------------------------------------------------
-    // forward_der
-    template<> void call_op_t<adten_t>::forward_der(
-        size_t                       op_index    ,
-        const agraph_t&              agraph      ,
-        const vector<at::Tensor>&    con_vec     ,
-        const vector<adten_t>&       par_vec     ,
-        const vector<adten_t>&       var_vec     ,
-        vector<adten_t>&             for_der
-    ) const {
-        assert(false && "call_op: ad_forward_der not implemented");
-    }
-    template<> void call_op_t<at::Tensor>::forward_der(
-        size_t                       op_index    ,
-        const agraph_t&              agraph      ,
-        const vector<at::Tensor>&    con_vec     ,
-        const vector<at::Tensor>&    par_vec     ,
-        const vector<at::Tensor>&    var_vec     ,
-        vector<at::Tensor>&          for_der
-    ) const {
-        assert(false && "call_op: forward_der not implemented");
-    }
-    // ------------------------------------------------------------------------
-    // reverse_der
-    template<> void call_op_t<adten_t>::reverse_der(
-        size_t                       op_index    ,
-        const agraph_t&              agraph      ,
-        const vector<at::Tensor>&    con_vec     ,
-        const vector<adten_t>&       par_vec     ,
-        const vector<adten_t>&       var_vec     ,
-        vector<adten_t>&             rev_der
-    ) const {
-        assert(false && "call_op: ad_reverse_der not implemented");
-    }
-    template<> void call_op_t<at::Tensor>::reverse_der(
-        size_t                       op_index    ,
-        const agraph_t&              agraph      ,
-        const vector<at::Tensor>&    con_vec     ,
-        const vector<at::Tensor>&    par_vec     ,
-        const vector<at::Tensor>&    var_vec     ,
-        vector<at::Tensor>&          rev_der
-    ) const {
-        assert(false && "call_op: reverse_der not implemented");
-    }
-} }
+namespace ad_tensor { namespace dev { // BEGIN_AD_TENSOR_DEV_NAMESPACE
+// ------------------------------------------------------------------------
+// forward_par
+template<> void call_op_t<adten_t>::forward_par(
+    size_t                       op_index    ,
+    const agraph_t&              agraph      ,
+    const vector<at::Tensor>&    con_vec     ,
+    vector<adten_t>&             par_vec
+) const {
+    assert(false && "call_op: ad_forward_par not implemented");
+}
+template<> void call_op_t<at::Tensor>::forward_par(
+    size_t                       op_index    ,
+    const agraph_t&              agraph      ,
+    const vector<at::Tensor>&    con_vec     ,
+    vector<at::Tensor>&          par_vec
+) const {
+    assert(false && "call_op: forward_par not implemented");
+}
+// ------------------------------------------------------------------------
+// forward_var
+template<> void call_op_t<adten_t>::forward_var(
+    size_t                       op_index    ,
+    const agraph_t&              agraph      ,
+    const vector<at::Tensor>&    con_vec     ,
+    const vector<adten_t>&       par_vec     ,
+    vector<adten_t>&             var_vec
+) const {
+    assert(false && "call_op: ad_forward_var not implemented");
+}
+template<> void call_op_t<at::Tensor>::forward_var(
+    size_t                       op_index    ,
+    const agraph_t&              agraph      ,
+    const vector<at::Tensor>&    con_vec     ,
+    const vector<at::Tensor>&    par_vec     ,
+    vector<at::Tensor>&          var_vec
+) const {
+    assert(false && "call_op: forward_var not implemented");
+}
+// ------------------------------------------------------------------------
+// forward_der
+template<> void call_op_t<adten_t>::forward_der(
+    size_t                       op_index    ,
+    const agraph_t&              agraph      ,
+    const vector<at::Tensor>&    con_vec     ,
+    const vector<adten_t>&       par_vec     ,
+    const vector<adten_t>&       var_vec     ,
+    vector<adten_t>&             for_der
+) const {
+    assert(false && "call_op: ad_forward_der not implemented");
+}
+template<> void call_op_t<at::Tensor>::forward_der(
+    size_t                       op_index    ,
+    const agraph_t&              agraph      ,
+    const vector<at::Tensor>&    con_vec     ,
+    const vector<at::Tensor>&    par_vec     ,
+    const vector<at::Tensor>&    var_vec     ,
+    vector<at::Tensor>&          for_der
+) const {
+    assert(false && "call_op: forward_der not implemented");
+}
+// ------------------------------------------------------------------------
+// reverse_der
+template<> void call_op_t<adten_t>::reverse_der(
+    size_t                       op_index    ,
+    const agraph_t&              agraph      ,
+    const vector<at::Tensor>&    con_vec     ,
+    const vector<adten_t>&       par_vec     ,
+    const vector<adten_t>&       var_vec     ,
+    vector<adten_t>&             rev_der
+) const {
+    assert(false && "call_op: ad_reverse_der not implemented");
+}
+template<> void call_op_t<at::Tensor>::reverse_der(
+    size_t                       op_index    ,
+    const agraph_t&              agraph      ,
+    const vector<at::Tensor>&    con_vec     ,
+    const vector<at::Tensor>&    par_vec     ,
+    const vector<at::Tensor>&    var_vec     ,
+    vector<at::Tensor>&          rev_der
+) const {
+    assert(false && "call_op: reverse_der not implemented");
+}
+} } // END_AD_TENSOR_DEV_NAMESPACE
