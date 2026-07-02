04e931e89e17e4afedf1dba8d572851b781c0354
Brad Bell <bradbell@seanet.com>
2026-07-01 18:29:35 -0700

    base_atom: ad_reverse_der.cpp: remove use of atom_callback_t.

--- examples/atom/ad_reverse_der.cpp
+++ examples/atom/ad_reverse_der.cpp
@@ -23,8 +23,11 @@ namespace {
     //
     // atom_id_z
     size_t atom_id_z;
+    // ----------------------------------------------------------------------
+    // y(x) = x * x * x
+    // ----------------------------------------------------------------------
     //
-    // derive_atom_y
+    // derive_atom_y_t
     class derive_atom_y_t : public base_atom_t {
     public:
         // ctor
@@ -98,8 +101,11 @@ namespace {
             return opt;
         }
     };
+    // ----------------------------------------------------------------------
+    // z(x, dy) = 3 * x * x * dy
+    // ----------------------------------------------------------------------
     //
-    // derive_atom_z
+    // derive_atom_z_t
     class derive_atom_z_t : public base_atom_t {
     public:
         // ctor
@@ -155,143 +161,13 @@ namespace {
             return opt;
         }
     };
+    // ------------------------------------------------------------------------
     //
     // base_atom_y_ptr, base_atom_z
     std::unique_ptr<base_atom_t> base_atom_y_ptr =
         std::make_unique<derive_atom_y_t>();
     std::unique_ptr<base_atom_t> base_atom_z_ptr =
         std::make_unique<derive_atom_z_t>();
-    //
-    // ----------------------------------------------------------------------
-    // y(x) = x * x * x
-    // ----------------------------------------------------------------------
-    // depend_y
-    std::optional<ad_tensor::sparsity_t> depend_y(
-        const options_t&      options   ,
-        size_t                call_info ) {
-        ad_tensor::sparsity_t sparsity;
-        std::array<size_t, 2> pair = {0, 0};
-        sparsity.push_back( pair );
-        //
-        std::optional<ad_tensor::sparsity_t> opt = sparsity;
-        return opt;
-    }
-    //
-    // forward
-    std::optional< vector<Tensor> > forward_y(
-        const options_t&      options   ,
-        size_t                call_info ,
-        const vector<bool>&   rng_used ,
-        const vector<Tensor>& domain ) {
-        //
-        Tensor x = domain[0];
-        //
-        // range
-        vector<Tensor> range;
-        range.push_back( x * x * x );
-        //
-        std::optional< vector<Tensor> > opt = range;
-        return opt;
-    }
-    //
-    // forward_der_y
-    std::optional< vector<Tensor> > forward_der_y(
-        const options_t&      options   ,
-        size_t                call_info ,
-        const vector<bool>&   rng_used  ,
-        const vector<Tensor>& domain    ,
-        const vector<Tensor>& dom_der   ) {
-        //
-        Tensor x  = domain[0];
-        Tensor dx = dom_der[0];
-        //
-        // range
-        vector<Tensor> rng_der;
-        rng_der.push_back( 3.0 * x * x * dx );
-        //
-        std::optional< vector<Tensor> > opt = rng_der;
-        return opt;
-    }
-    //
-    // ad_reverse_der_y
-    std::optional< vector<adten_t> > ad_reverse_der_y(
-        const options_t&       options   ,
-        size_t                 call_info ,
-        const vector<bool>&    rng_used ,
-        const vector<adten_t>& domain   ,
-        const vector<adten_t>& rng_der ) {
-        //
-        if( rng_der[0].numel() == 0 ) {
-            return rng_der;
-        }
-        //
-        vector<adten_t> domain_z;
-        domain_z.push_back( domain[0] );
-        domain_z.push_back( rng_der[0] );
-        //
-        // dom_der
-        vector<adten_t> dom_der = adten_t::call_atom(
-            atom_id_z, call_info, domain_z
-        );
-        //
-        std::optional< vector<adten_t> > opt = dom_der;
-        return opt;
-    }
-    // ----------------------------------------------------------------------
-    // z(x, dy) = 3 * x * x * dy
-    // ----------------------------------------------------------------------
-    // depend_z
-    std::optional<ad_tensor::sparsity_t> depend_z(
-        const options_t&      options   ,
-        size_t                call_info ) {
-        ad_tensor::sparsity_t sparsity;
-        sparsity.push_back( {0, 0} );
-        sparsity.push_back( {0, 1} );
-        //
-        std::optional<ad_tensor::sparsity_t> opt = sparsity;
-        return opt;
-    }
-    //
-    // forward_z
-    std::optional< vector<Tensor> > forward_z(
-        const options_t&      options   ,
-        size_t                call_info ,
-        const vector<bool>&   rng_used ,
-        const vector<Tensor>& domain ) {
-        //
-        Tensor x  = domain[0];
-        Tensor dy = domain[1];
-        Tensor z  = 3.0 * x * x * dy;
-        // range
-        vector<Tensor> range;
-        range.push_back( z );
-        //
-        std::optional< vector<Tensor> > opt = range;
-        return opt;
-    }
-    //
-    // forward_der_z
-    std::optional< vector<Tensor> > forward_der_z(
-        const options_t&      options   ,
-        size_t                call_info,
-        const vector<bool>&   rng_used,
-        const vector<Tensor>& domain,
-        const vector<Tensor>& dom_der) {
-        //
-        // dz
-        Tensor x    = domain[0];
-        Tensor dy   = domain[1];
-        Tensor d_x  = dom_der[0];
-        Tensor d_dy = dom_der[1];
-        Tensor dz  = 6.0 * x * dy * d_x + 3.0 * x * x * d_dy;
-        //
-        // rng_der
-        vector<Tensor> rng_der;
-        rng_der.push_back( dz );
-        //
-        std::optional< vector<Tensor> > opt = rng_der;
-        return opt;
-    }
 }
 TEST(examples_atom, ad_reverse_der)  {
     //
@@ -299,23 +175,8 @@ TEST(examples_atom, ad_reverse_der)  {
     ad_tensor::atom_global_t& atom_global =
         ad_tensor::atom_global_t::singleton();
     //
-    // atom_callback_z
-    ad_tensor::atom_callback_t atom_callback_z;
-    atom_callback_z.set_name("z");
-    atom_callback_z.set_depend(depend_z);
-    atom_callback_z.set_forward(forward_z);
-    atom_callback_z.set_forward_der(forward_der_z);
-    //
     // atom_id_z
     atom_id_z = atom_global.store(base_atom_z_ptr);
-    //
-    // atom_callback_y
-    ad_tensor::atom_callback_t atom_callback_y;
-    atom_callback_y.set_name("y");
-    atom_callback_y.set_depend(depend_y);
-    atom_callback_y.set_forward(forward_y);
-    atom_callback_y.set_forward_der(forward_der_y);
-    atom_callback_y.set_ad_reverse_der(ad_reverse_der_y);
     //
     // atom_id_y
     size_t atom_id_y = atom_global.store(base_atom_y_ptr);
