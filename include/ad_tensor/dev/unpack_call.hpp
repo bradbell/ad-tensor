f35ceb0379fc4de3c92c4453c16b4fb10ee90897
Brad Bell <bradbell@seanet.com>
2026-07-14 19:04:40 -0700

    main: unpack_call.hpp: change std::tuple -> std::array.

--- include/ad_tensor/dev/unpack_call.hpp
+++ include/ad_tensor/dev/unpack_call.hpp
@@ -4,13 +4,13 @@
 // SPDX-FileContributor: 2026 Bradley M. Bell
 // ----------------------------------------------------------------------------
 #include <cstddef>
-#include <tuple>
+#include <array>
 #include <ad_tensor/dev/agraph.hpp>
 //
 namespace ad_tensor { namespace dev {
     //
     // unpack_call
-    inline std::tuple<size_t, size_t, size_t, size_t, size_t> unpack_call(
+    inline std::array<size_t, 5> unpack_call(
         size_t         op_index,
         const agraph_t agraph  ) {
         size_t arg_start = agraph.m_arg_start[op_index];
@@ -18,8 +18,8 @@ namespace ad_tensor { namespace dev {
         size_t n_domain  = agraph.m_arg_value[arg_start + 1];
         size_t n_range   = agraph.m_arg_value[arg_start + 2];
         size_t n_result  = agraph.m_arg_value[arg_start + 3];
-        return std::tuple<size_t, size_t, size_t, size_t, size_t> (
-            arg_start, atom_id, n_domain, n_range, n_result
+        return std::array<size_t, 5> (
+            { arg_start, atom_id, n_domain, n_range, n_result }
         );
     }
 } }
