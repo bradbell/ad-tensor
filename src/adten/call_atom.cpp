1f78bf5b41803f7a790b574f15c761d751f2301d
Brad Bell <bradbell@seanet.com>
2026-07-18 23:00:20 -0700

    main: call_atom.cpp: use nan for domain values that are not used.

--- src/adten/call_atom.cpp
+++ src/adten/call_atom.cpp
@@ -207,10 +207,9 @@ vector<adten_t> adten_t::call_atom(
         for(size_t j = 0; j < n_domain; ++j) {
             ad_type_t ad_type = adomain[j].m_ad_type;
             if( ig == 0 && ad_type == ad_type_t::variable ) {
-                adten_t acon = adten_t( adomain[j].m_at_ten );
-                assert( acon.m_ad_type == ad_type_t::constant );
-                agraph->m_arg_value.push_back( acon.m_index );
-                agraph->m_arg_type.push_back( acon.m_ad_type );
+                // nan
+                agraph->m_arg_value.push_back( 0 );
+                agraph->m_arg_type.push_back( ad_type_t::constant );
             } else {
                 agraph->m_arg_value.push_back( adomain[j].m_index );
                 agraph->m_arg_type.push_back( adomain[j].m_ad_type );
