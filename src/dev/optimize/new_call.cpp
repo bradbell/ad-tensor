eb07111960ba14c4560f3bc1af71b5e9ea3d38fb
Brad Bell <bradbell@seanet.com>
2026-07-30 14:25:37 -0700

    main: new_call.cpp: white space change for next edit.

--- src/dev/optimize/new_call.cpp
+++ src/dev/optimize/new_call.cpp
@@ -99,63 +99,65 @@ size_t new_call(
     auto [arg_start, atom_id, n_domain, n_range, n_result_old] = unpack_call(
         op_index_old, agraph_old
     );
-    //
-    // n_result_new
-    size_t n_result_new  = 0;
-    for(size_t k = 0; k < n_result_old; ++k) {
-        old2new[op_index_old + k] = not_used;
-        if( depend_old[op_index_old + k] ) {
-            size_t op_index_new = agraph_new.m_op_seq.size() + n_result_new;
-            old2new[op_index_old + k]  = op_index_new;
-            ++n_result_new;
-        }
-    }
-    assert( 0 < n_result_new );
-    //
-    // agraph_new: m_op_seq, m_arg_start
-    agraph_new.m_op_seq.push_back( dev::op_enum_t::call );
-    agraph_new.m_arg_start.push_back( agraph_new.m_arg_value.size() );
-    //
-    // agraph_new: m_arg_value, m_arg_type
-    agraph_new.m_arg_value.push_back( atom_id );
-    agraph_new.m_arg_value.push_back( n_domain );
-    agraph_new.m_arg_value.push_back( n_range );
-    agraph_new.m_arg_value.push_back( n_result_new );
-    for(size_t k = 0; k < 4; ++k) {
-        agraph_new.m_arg_type.push_back( ad_type_t::none );
-    }
-    // agraph_new: m_arg_value, m_arg_type
-    for(size_t j = 0; j < n_domain; ++j) {
-        size_t    arg_index     = arg_start + 4 + j;
-        size_t    arg_value_old = agraph_old.m_arg_value[arg_index];
-        ad_type_t arg_type      = agraph_old.m_arg_type[arg_index];
-        if( arg_type != agraph_type ) {
-            agraph_new.m_arg_value.push_back( arg_value_old );
-        } else {
-            if( ! depend_old[arg_value_old] ) {
-                // empty_at_ten()
-                agraph_new.m_arg_value.push_back( 0 );
-                arg_type = ad_type_t::constant;
-            } else {
-                agraph_new.m_arg_value.push_back( old2new[arg_value_old] );
+    {
+        //
+        // n_result_new
+        size_t n_result_new  = 0;
+        for(size_t k = 0; k < n_result_old; ++k) {
+            old2new[op_index_old + k] = not_used;
+            if( depend_old[op_index_old + k] ) {
+                size_t op_index_new = agraph_new.m_op_seq.size()+n_result_new;
+                old2new[op_index_old + k]  = op_index_new;
+                ++n_result_new;
             }
         }
-        agraph_new.m_arg_type.push_back( arg_type );
-    }
-    // agraph_new: m_arg_value, m_arg_type
-    for(size_t k = 0; k < n_result_old; ++k) {
-        if( depend_old[op_index_old + k] ) {
-            size_t arg_index = arg_start + 4 + n_domain + k;
-            size_t arg_value = agraph_old.m_arg_value[ arg_index ];
-            agraph_new.m_arg_value.push_back( arg_value );
+        assert( 0 < n_result_new );
+        //
+        // agraph_new: m_op_seq, m_arg_start
+        agraph_new.m_op_seq.push_back( dev::op_enum_t::call );
+        agraph_new.m_arg_start.push_back( agraph_new.m_arg_value.size() );
+        //
+        // agraph_new: m_arg_value, m_arg_type
+        agraph_new.m_arg_value.push_back( atom_id );
+        agraph_new.m_arg_value.push_back( n_domain );
+        agraph_new.m_arg_value.push_back( n_range );
+        agraph_new.m_arg_value.push_back( n_result_new );
+        for(size_t k = 0; k < 4; ++k) {
             agraph_new.m_arg_type.push_back( ad_type_t::none );
         }
-    }
-    //
-    // agraph: m_op_seq, m_arg_start
-    for(size_t k = 1; k < n_result_new; ++k) {
-        agraph_new.m_op_seq.push_back( dev::op_enum_t::call_result );
-        agraph_new.m_arg_start.push_back( agraph_new.m_arg_value.size() );
+        // agraph_new: m_arg_value, m_arg_type
+        for(size_t j = 0; j < n_domain; ++j) {
+            size_t    arg_index     = arg_start + 4 + j;
+            size_t    arg_value_old = agraph_old.m_arg_value[arg_index];
+            ad_type_t arg_type      = agraph_old.m_arg_type[arg_index];
+            if( arg_type != agraph_type ) {
+                agraph_new.m_arg_value.push_back( arg_value_old );
+            } else {
+                if( ! depend_old[arg_value_old] ) {
+                    // empty_at_ten()
+                    agraph_new.m_arg_value.push_back( 0 );
+                    arg_type = ad_type_t::constant;
+                } else {
+                    agraph_new.m_arg_value.push_back( old2new[arg_value_old] );
+                }
+            }
+            agraph_new.m_arg_type.push_back( arg_type );
+        }
+        // agraph_new: m_arg_value, m_arg_type
+        for(size_t k = 0; k < n_result_old; ++k) {
+            if( depend_old[op_index_old + k] ) {
+                size_t arg_index = arg_start + 4 + n_domain + k;
+                size_t arg_value = agraph_old.m_arg_value[ arg_index ];
+                agraph_new.m_arg_value.push_back( arg_value );
+                agraph_new.m_arg_type.push_back( ad_type_t::none );
+            }
+        }
+        //
+        // agraph: m_op_seq, m_arg_start
+        for(size_t k = 1; k < n_result_new; ++k) {
+            agraph_new.m_op_seq.push_back( dev::op_enum_t::call_result );
+            agraph_new.m_arg_start.push_back( agraph_new.m_arg_value.size() );
+        }
     }
     //
     return op_index_old + n_result_old;
