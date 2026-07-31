c96d0483bf20f7e3065ee4e1c2e2b3076a95abc8
Brad Bell <bradbell@seanet.com>
2026-07-30 18:21:29 -0700

    main: test: optimize.cpp: check n_con, n_par, and n_var.

--- tests/adfn/optimize.cpp
+++ tests/adfn/optimize.cpp
@@ -140,9 +140,9 @@ TEST(tests_adfn, optimize_call)  {
     //
     // ar
     vector<adten_t> ar;
-    ar.push_back( av[0] );
-    ar.push_back( av[1] );
-    ar.push_back( av[2] );
+    ar.push_back( av[0] );  // v_all[0]
+    ar.push_back( av[1] );  // v_all[1]
+    ar.push_back( av[2] );  // v_all[2]
     //
     // r = f(v)
     adfn_t f           = adten_t::stop_recording(ar, "f");
@@ -150,38 +150,50 @@ TEST(tests_adfn, optimize_call)  {
     //
     // p
     vector<Tensor> p;
-    p.push_back( torch::tensor( {2.0, 2.0} ) );
+    p.push_back( torch::tensor( {2.0, 2.0} ) );  // p_all[0]
     //
     // av, ap
     vector<adten_t> ap;
     std::tie(av, ap) = adten_t::start_recording(v, p);
     //
     // z = g(v, p)
-    vector<adten_t> ax = { ap[0], av[0] * av[1], av[1] + av[2] };
+    adten_t used_par = ap[0] + ap[0];  // p_all[1]
+    adten_t used_var = av[0] * av[1];  // v_all[3]
+    adten_t not_used_var = av[1] + av[2];  // v_all[4]
+    vector<adten_t> ax = { used_par, used_var, not_used_var };
+    //
+    // ay = p_all[2], v_all[5], v_all[6] (v_all[6] is not used)
     vector<adten_t> ay = ad_tensor::call_chkpnt(f_chkpnt_id, ax);
+    //
     vector<adten_t> az = { ay[0], ay[1] };
     adfn_t          g  = adten_t::stop_recording(az, "g");
+    EXPECT_EQ(g.n_con(), 1);
+    EXPECT_EQ(g.n_par(), 3);
+    EXPECT_EQ(g.n_var(), 7);
     //
     // z
     vector<Tensor> p_all = g.forward_par(p);
     vector<Tensor> v_all = g.forward_var(v, p_all);
     vector<Tensor> z     = g.get_range(v_all, p_all);
     //
-    bool equal = z[0].equal( p[0] );
+    bool equal = z[0].equal( p[0] + p[0] );
     EXPECT_TRUE( equal );
     //
     equal = z[1].equal( v[0] * v[1] );
     EXPECT_TRUE( equal );
     //
-    // z = g(v, p)
-    g  = g.optimize();
+    // h = g
+    adfn_t h  = g.optimize();
+    EXPECT_EQ(h.n_con(), 1);
+    EXPECT_EQ(h.n_par(), 3);
+    EXPECT_EQ(h.n_var(), 5);
     //
     // z
-    p_all = g.forward_par(p);
-    v_all = g.forward_var(v, p_all);
-    z     = g.get_range(v_all, p_all);
+    p_all = h.forward_par(p);
+    v_all = h.forward_var(v, p_all);
+    z     = h.get_range(v_all, p_all);
     //
-    equal = z[0].equal( p[0] );
+    equal = z[0].equal( p[0] + p[0] );
     EXPECT_TRUE( equal );
     //
     equal = z[1].equal( v[0] * v[1] );
@@ -194,7 +206,7 @@ TEST(tests_adfn, optimize_call)  {
     dv.push_back( torch::tensor( {5.0, 6.0} ) );
     //
     // dz
-    vector<Tensor> dz = g.forward_der(dv, v_all, p_all);
+    vector<Tensor> dz = h.forward_der(dv, v_all, p_all);
     //
     equal = dz[0].equal( torch::empty( {0} ) );
     EXPECT_TRUE(equal);
