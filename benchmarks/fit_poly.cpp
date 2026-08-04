e7a2b6561a0dfaf710670ea1f230c779232b0ce2
Brad Bell <bradbell@seanet.com>
2026-08-04 15:51:46 -0700

    main: fit_poly.cpp : refactor in preparation for ad_tensor version.

--- benchmarks/fit_poly.cpp
+++ benchmarks/fit_poly.cpp
@@ -5,49 +5,62 @@
 #include <chrono>
 #include <gtest/gtest.h>
 #include <torch/torch.h>
+#include <ad_tensor/ad_tensor.hpp>
+
+namespace {
+    //
+    // Tensor
+    using torch::Tensor;
+    using ad_tensor::vector;
+    //
+    // loss
+    Tensor loss(const vector<Tensor>& c, const Tensor& y, const Tensor& x) {
+        assert( y.numel() == x.numel() );
+        int64_t n_y = y.numel();
+        size_t n_c  = c.size();
+        Tensor xp      = torch::ones( {n_y} );
+        Tensor predict = c[0] * xp;
+        for(size_t j = 1; j < n_c; ++j) {
+            xp       = xp * x;
+            predict  = predict + c[j] * xp;
+        }
+        Tensor residual = (y - predict);
+        return (residual * residual).sum();
+    }
+}
 
 // https://docs.pytorch.org/tutorials/beginner/
 //  pytorch_with_examples.html#pytorch-tensors-and-autograd
 TEST(benchmarks, fit_poly_autograd) {
-    using torch::Tensor;
     //
     // x, y
     Tensor x = torch::linspace(-1.0, 1.0, 2000);
     Tensor y = x.exp();
     //
-    // a, b, c, d
-    Tensor a = torch::randn( {1}, torch::requires_grad() );
-    Tensor b = torch::randn( {1}, torch::requires_grad() );
-    Tensor c = torch::randn( {1}, torch::requires_grad() );
-    Tensor d = torch::randn( {1}, torch::requires_grad() );
-    //
-    // x2, x3
-    Tensor x2 = x * x;
-    Tensor x3 = x * x2;
+    // c
+    size_t n_c = 4;
+    vector<Tensor> c;
+    for(size_t j = 0; j < n_c; ++j) {
+        c.push_back( torch::randn( {1}, torch::requires_grad() ) );
+    }
     //
     // learning_rage, t
     double initial_loss  = 1.0;
     double relative_loss = 1.0;
     double learning_rate = 1e-5;
     for(size_t t = 0; t < 5000; ++t) {
-        Tensor y_pred = a + b * x + c * x2 + d * x3;
-        Tensor loss   = ( (y_pred - y) * (y_pred - y) ).sum();
+        Tensor loss_t = loss(c, y, x);
         if( t == 0 ) {
-            initial_loss = loss.item<double>();
+            initial_loss = loss_t.item<double>();
         }
-        relative_loss = loss.item<double>() / initial_loss;
-        loss.backward();
+        relative_loss = loss_t.item<double>() / initial_loss;
+        loss_t.backward();
         {   torch::NoGradGuard no_grad;
             //
-            a -= learning_rate * a.grad();
-            b -= learning_rate * b.grad();
-            c -= learning_rate * c.grad();
-            d -= learning_rate * d.grad();
-            //
-            a.grad().zero_();
-            b.grad().zero_();
-            c.grad().zero_();
-            d.grad().zero_();
+            for(size_t j = 0; j < n_c; ++j) {
+                c[j] -= learning_rate * c[j].grad();
+                c[j].grad().zero_();
+            }
         }
     }
     //
