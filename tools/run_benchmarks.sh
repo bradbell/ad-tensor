49cc17f01c90e338efce1e988f309bac1d8b85be
Brad Bell <bradbell@seanet.com>
2026-09-08 07:32:55 -0700

    main: run_benchmarks.sh: improve automatic reporting of system properties.

--- tools/run_benchmarks.sh
+++ tools/run_benchmarks.sh
@@ -9,11 +9,24 @@ script_dir="$( dirname -- "${BASH_SOURCE[0]}" )"
 script_dir="$( cd -- "$script_dir" &> /dev/null && pwd )"
 script_path="$script_dir/$(basename $0)"
 # -----------------------------------------------------------------------------
-program='build/benchmarks/benchmarks'
-$program --gtest_filter=benchmarks.version  | sed -n \
-    -e '/^ad-tensor version/p' \
-    -e '/^cmake build type/p'
-$program --gtest_filter=*:-benchmarks.version  | sed -n \
+cd build
+ninja benchmarks
+cd ..
+echo "machine = $(uname -m)"
+echo "kernel version = $(uname -r)"
+echo "operating system - $(uname -o)"
+#
+configure='build/include/ad_tensor/configure.hpp'
+compiler=$(grep CMAKE_CXX_COMPILER $configure | sed -e 's|.*COMPILER *||' )
+$compiler --version | head -1
+#
+grep "CMAKE_BUILD_TYPE"  $configure
+grep "AD_TENSOR_VERSION" $configure
+grep "INCLUDE_PLUGIN"    $configure
+#
+#
+benmchmarks='build/benchmarks/benchmarks'
+$benmchmarks | sed -n \
     -e '/OK *\]/p' \
     -e '/^learn_ms =/p'
 # -----------------------------------------------------------------------------
